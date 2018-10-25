20bb8d051d0a675932dd60a44977bec0e206dd21
AgnieszkaS-G <surazynskaa@gmail.com>
2018-10-25 19:19:48 +0200

    import

---
+++ formularzzbaza.php
@@ -0,0 +1,14 @@
+
+<html>
+<form action="mailto:twój_adres_poczty@domena" method="post" enctype="text/plain"><div>
+<!-- Podstawowe pole tekstowe -->
+<input type="text" id="name"  />Podaj swoje imię<br />
+<input type="text" id="surname" />Podaj swoje nazwisko<br/>
+<input type="email" id= "email" /> podaj e-mail <br/>
+<input type="password" id="password" /> Podaj hasło <br />
+<input type="password" id="verpassword" /> Powtórz hasło <br/>
+<input type="submit">
+</form>
+</html>
+
+
